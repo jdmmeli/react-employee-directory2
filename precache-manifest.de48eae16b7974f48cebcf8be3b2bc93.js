d53516eb77b1664035c3f708532aba8ede4e1fd1
Jason Meli <jdmmeli@yahoo.com>
2020-07-02 12:09:01 -0400

    Updates

---
+++ precache-manifest.de48eae16b7974f48cebcf8be3b2bc93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14281929e6ddf4c202ad55a721394d79",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/index.html"
+  },
+  {
+    "revision": "32fb19d3ad69bc9279fd",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "76edf561bc9ce7b6707b",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/static/js/2.7fdd39c6.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/static/js/2.7fdd39c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fb19d3ad69bc9279fd",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/static/js/main.d1bdc2be.chunk.js"
+  },
+  {
+    "revision": "666d531e69fa317541f6",
+    "url": "/http//jdmmeli.github.io/react-employee-directory2/static/js/runtime-main.e1d756ed.js"
+  }
+]);
